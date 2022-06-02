7d8090f62a98bd1d4135b948a5a282ffd0906a4e
Allan Ndonga <93614371+alz254@users.noreply.github.com>
2022-06-02 13:56:03 +0300

    Create 101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+   * main - prints to the standard error
+   *
+   * Return: one
+   */
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
