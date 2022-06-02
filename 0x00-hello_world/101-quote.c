f9cc87bae945cf3deb41df9e8060ff72307950bc
Allan Ndonga <93614371+alz254@users.noreply.github.com>
2022-06-02 13:59:52 +0300

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,15 @@
-#include <stdio.h>
+#include <unistd.h>
 /**
-   * main - prints to the standard error
-   *
-   * Return: one
-   */
+ * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
+ *                followed by a new line, to standard error.
+ *
+ * Return: Always 1.
+ */
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	write(2,
+	      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+	      59);
+
 	return (1);
 }
