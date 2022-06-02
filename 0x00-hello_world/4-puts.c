b8034e6882d5becbfc59527794a49eb68ea827dc
Allan Ndonga <93614371+alz254@users.noreply.github.com>
2022-06-02 13:05:57 +0300

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,21 +1,11 @@
 #include <stdio.h>
-
 /**
-
  * main - Entry point
-
  *
-
  * Return: always 0
-
  */
-
 int main(void)
-  
 {
-  
-  printf("\"with proper grammar, but the outcome is a piece of art,");
-  
-  return (0);
-  
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
