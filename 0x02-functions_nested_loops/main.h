662e27ff6513a7228272845d468cd8fa427c80ce
Allan Ndonga <93614371+alz254@users.noreply.github.com>
2022-06-08 08:37:21 +0300

    Update main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -6,6 +6,7 @@ int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
+void times_table(void);
 void jack_bauer(void);
 void print_times_table(int n);
 int add(int, int);
