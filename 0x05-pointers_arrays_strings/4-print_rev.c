00277cf96190f5c5bda2641a459b32aaabae6569
Allan Ndonga <93614371+alz254@users.noreply.github.com>
2022-06-13 12:32:47 +0300

    Create 4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse.
+ *
+ * @s: store the string.
+ */
+void print_rev(char *s)
+{
+	int i;
+
+	i = string_length(s) - 1;
+	while (i >= 0)
+	{
+		_putchar(s[i]);
+		i--;
+	}
+	_putchar('\n');
+}
+
+/**
+  * string_length - finds the length of a string.
+  * Return: length of c.
+  * @pointer: pointer.
+  */
+int string_length(char *pointer)
+{
+	int c = 0;
+
+	while (*(pointer + c) != '\0')
+	{
+		c++;
+	}
+	return (c);
+}
+© 2022 GitHub, Inc.
+Terms
