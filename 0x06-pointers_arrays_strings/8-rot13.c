af72f6994fd024a3552d6d250dd7f7cac3c5329b
Allan Ndonga <93614371+alz254@users.noreply.github.com>
2022-06-15 18:32:58 +0300

    Update 8-rot13.c

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -1,46 +1,26 @@
 #include "main.h"
-
 /**
- * rot13 - Encodes a string using rot13.
- * @str: The string to be encoded.
+ * *rot13 - encodes a string using rot13.
  *
- * Return: A pointer to the encoded string.
+ * @str: string.
+ * Return: string.
  */
 char *rot13(char *str)
 {
-	int indx1 = 0, indx2;
-	char alphabet[52] = {'A', 'B', 'C', 'D', 'E', 'F',
-			     'G', 'H', 'I', 'J', 'K', 'L',
-			     'M', 'N', 'O', 'P', 'Q', 'R',
-			     'S', 'T', 'U', 'V', 'W', 'X',
-			     'Y', 'Z', 'a', 'b', 'c', 'd',
-			     'e', 'f', 'g', 'h', 'i', 'j',
-			     'k', 'l', 'm', 'n', 'o', 'p',
-			     'q', 'r', 's', 't', 'u', 'v',
-			     'w', 'x', 'y', 'z'};
-	char rot13key[52] = {'N', 'O', 'P', 'Q', 'R', 'S',
-			     'T', 'U', 'V', 'W', 'X', 'Y',
-			     'Z', 'A', 'B', 'C', 'D', 'E',
-			     'F', 'G', 'H', 'I', 'J', 'K',
-			     'L', 'M', 'n', 'o', 'p', 'q',
-			     'r', 's', 't', 'u', 'v', 'w',
-			     'x', 'y', 'z', 'a', 'b', 'c',
-			     'd', 'e', 'f', 'g', 'h', 'i',
-			     'j', 'k', 'l', 'm'};
+	int i;
 
-	while (str[indx1])
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		for (indx2 = 0; indx2 < 52; indx2++)
+		if ((*(str + i) >= 'a' && *(str + i) < 'n')
+				|| (*(str + i) >= 'A' && *(str + i) < 'N'))
 		{
-			if (str[indx1] == alphabet[indx2])
-			{
-				str[indx1] = rot13key[indx2];
-				break;
-			}
+			*(str + i) += 13;
 		}
-
-		indx1++;
+		else if ((*(str + i) >= 'n' && *(str + i) <= 'z')
+				|| (*(str + i) >= 'N' && *(str + i) <= 'Z'))
+		{
+			*(str + i) -= 13;
+		}
 	}
-
 	return (str);
 }
