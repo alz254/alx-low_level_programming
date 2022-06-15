caae3280b722322bce6a922e4250b63822e9eb8c
Allan Ndonga <93614371+alz254@users.noreply.github.com>
2022-06-15 19:08:18 +0300

    Update main.h

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -12,3 +12,4 @@ char *rot13(char *);
 void print_number(int n);
 char *add_strings(char *n1, char *n2, char *r, int r_index);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size)
