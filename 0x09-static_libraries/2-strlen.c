e9d8c7bae0f57e7cafdf6c7fd67c8f254bc6a351
Allan Ndonga <rezidentalz@gmail.com>
2022-06-24 22:51:31 +0300

    new

---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string.
+ *
+ * @s: hold each character after the count
+ * Return: length of a string.
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	}
+	return (i);
+}
---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _islower - checks for lowercase characters.
+ *
+ * @c: character to be checked
+ * Return: Always 0.
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	return (0);
+}
