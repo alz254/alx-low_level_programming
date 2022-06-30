5fbbbb16a509940c4f58f7101a062a9ad0f526a7
Allan Ndonga <93614371+alz254@users.noreply.github.com>
2022-06-30 14:03:33 +0300

    Update 101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -9,7 +9,7 @@
  */
 int main(int argc, char **argv)
 {
-	int num1, num2, result;
+	int num1, num2, mul;
 
 	if (argc != 3)
 	{
@@ -21,7 +21,7 @@ int main(int argc, char **argv)
 		num1 = atoi(argv[1]);
 		num2 = atoi(argv[2]);
 		result = num1 * num2;
-		printf("%d\n", result);
+		printf("%d\n", mul);
 		return (0);
 		}
 }
