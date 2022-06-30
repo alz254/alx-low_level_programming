692f08ade74c6599cec43ac36aa8ca59ad971ae6
Allan Ndonga <93614371+alz254@users.noreply.github.com>
2022-06-30 14:58:44 +0300

    Delete 0x0C-more_malloc_free directory

--- 0x0C-more_malloc_free/main.h
+++
@@ -1,9 +0,0 @@
-#ifndef _MAIN_H
-#define _MAIN_H
-
-void *malloc_checked(unsigned int b);
-char *string_nconcat(char *s1, char *s2, unsigned int n);
-void *_calloc(unsigned int nmemb, unsigned int size);
-int *array_range(int min, int max);
-
-#endif
