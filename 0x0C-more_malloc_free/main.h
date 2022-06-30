3b7398d4a4d93b212734ff8f317b9491ab59c2f3
Allan Ndonga <93614371+alz254@users.noreply.github.com>
2022-06-30 12:56:16 +0300

    Update main.h

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -7,4 +7,7 @@ int string_length(char *pointer);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+int _atoi(const char *s);
+void print_int(unsigned long int n);
+void _puts(char *str);
 #endif
